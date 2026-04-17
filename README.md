9ce18deeb9aab086e0ccfe0a55048e324d8aa5ba
frutabruta <adameichler1@gmail.com>
2026-04-17 18:59:46 +0200

    Vdv301ViaPoint2_3CZ1_0 usage

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 Library of data storage classes for use with VDV301
 
 ## Changelog
+- 20260417_1858
+  - Vdv301DisplayContent2_3CZ1_0
+    - added QVector<Vdv301ViaPoint2_3CZ1_0> viaPointList
+    
 - 20260401_1509
   - StopPoint::secondsToQtime
     - added option to return empty time
--- vdv301displaycontent2_3cz1_0.h
+++ vdv301displaycontent2_3cz1_0.h
@@ -2,11 +2,13 @@
 #define VDV3_1DISPLAYCONTENT2_3CZ1___H
 
 #include "vdv301displaycontent.h"
+#include "vdv301viapoint2_3cz1_0.h"
 
 class Vdv301DisplayContent2_3CZ1_0 : public Vdv301DisplayContent
 {
 public:
     Vdv301DisplayContent2_3CZ1_0();
+    QVector<Vdv301ViaPoint2_3CZ1_0> viaPointList; // minOccurs="0" maxOccurs="unbounded",Information on the via points which should be displayed
 };
 
 #endif // VDV3_1DISPLAYCONTENT2_3CZ1___H
